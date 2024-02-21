66a03936a82711c215be0751b77450c7ff3f2df6
Julien Schueller <schueller@phimeca.com>
2024-02-21 14:46:38 +0100

    Set QT_HOST_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,13 @@ then
   CMAKE_ARGS="${CMAKE_ARGS} -DQT_HOST_PATH=${BUILD_PREFIX}"
 fi
 
-cmake -LAH -G "Ninja" ${CMAKE_ARGS} \
+cmake -LAH -G "Ninja" \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_FIND_FRAMEWORK=LAST \
   -DCMAKE_INSTALL_RPATH:STRING=${PREFIX}/lib \
   -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
   -DCMAKE_MESSAGE_LOG_LEVEL=STATUS \
-  -B build .
+  -DQT_HOST_PATH=${PREFIX} \
+  -B build ${CMAKE_ARGS} .
 cmake --build build --target install
 
